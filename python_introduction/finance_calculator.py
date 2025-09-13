98a7feb85c17acba85b5dcdd6097cad9791e2b4e
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-13 06:19:45 +0100

    finace calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 # this is basic finance calculator project based on one's total income and expenses in a month
-monthly_income = int(input("Enter monthly income"))
-monthly_expenses = int(input("Enter total monthly expenses"))
-monthly_savings = monthly_expenses - monthly_income 
+monthly_income = float(input("Enter monthly income"))
+monthly_expenses = float(input("Enter total monthly expenses"))
+monthly_savings = monthly_income - monthly_expenses
 print(f"Your monthly savings  are, {monthly_savings}")
 
 #The projected savings in a year based on an interest rate of five percent 

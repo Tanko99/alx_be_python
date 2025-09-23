6ed22f984dcd07d62c82bf908af16876ba155640
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-23 08:33:00 +0100

    a simple weather advice script using if, elif and else

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,17 @@
+#This script gives a weather advice based on weather conditions
+
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses")
+
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat")
+
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf")
+
+else:
+    print("Sorry, I don't have recommendation for this weather")
+
+
+    

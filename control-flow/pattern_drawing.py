3167d58d455b4bef3b3bcb3c74d9bfcb443b6857
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-26 17:57:11 +0100

    daily reminder script using match case and if/else

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,11 @@
 ##Drawing a pattern using nested loops
 
-size = in(input("Enter the size of the pattern::"))
+size = int(input("Enter the size of the pattern::"))
 
-i = 0 
-while i < size:
-    for j in range(size):
-        print("*", end="")
+row = 0 
+while row < size:
+    for columns in range(size):
+        print("*" end="")
     print()
-    i += 1
+    row += 1
+    

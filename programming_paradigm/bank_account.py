59c045b3db0fa3be5e27813606ee488592f65c66
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-10-05 17:29:05 +0100

    bank account and robust divsion fixed

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,12 +2,10 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
 
-    
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
             
-
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
